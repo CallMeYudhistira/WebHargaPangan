33854bb1014e700c6b657f031ec81c38ffb7bfd2
CallMeYudhistira <yudhistira30122008@gmail.com>
2025-06-25 21:38:01 +0700

    fix navbar

--- pages/user/includes/navbar.php
+++ pages/user/includes/navbar.php
@@ -4,30 +4,32 @@ $current_page = trim($_GET['route'] ?? "", "/");
 
 ?>
 
-<header class="app-bar">
-    <div class="d-flex m-auto">
-        <img src="assets/img/Logo-Cimahi.PNG" alt="Logo Cimahi" class="icon-nav">
-        <h1>INFO PANGAN KOTA CIMAHI</h1>
+<header style="border-bottom: 1px solid #ccc; max-height: 100px; margin-top: -10px;">
+    <div class="container app-bar">
+        <div class="d-flex m-auto">
+            <img src="assets/img/Logo-Cimahi.PNG" alt="Logo Cimahi" class="icon-nav">
+            <h1>INFO PANGAN KOTA CIMAHI</h1>
+        </div>
+        <nav class="nav-menu">
+            <ul class="nav-item">
+                <li>
+                    <a href="index.php" class="nav-link <?= ($current_page == '') ? 'active' : ''; ?>">Beranda</a>
+                </li>
+                <div class="vertical-line"></div>
+                <li>
+                    <a href="index.php?route=/komoditas"
+                        class="nav-link <?= ($current_page == 'komoditas') ? 'active' : ''; ?>">Komoditas</a>
+                </li>
+                <div class="vertical-line"></div>
+                <li>
+                    <a href="index.php?route=/statistik"
+                        class="nav-link <?= ($current_page == 'statistik') ? 'active' : ''; ?>">Statistik</a>
+                </li>
+                <div class="vertical-line"></div>
+                <li>
+                    <a href="index.php?route=/admin" class="nav-link">Panel Admin</a>
+                </li>
+            </ul>
+        </nav>
     </div>
-    <nav class="nav-menu">
-        <ul class="nav-item">
-            <li>
-                <a href="index.php" class="nav-link <?= ($current_page == '') ? 'active' : ''; ?>">Beranda</a>
-            </li>
-            <div class="vertical-line"></div>
-            <li>
-                <a href="index.php?route=/komoditas"
-                    class="nav-link <?= ($current_page == 'komoditas') ? 'active' : ''; ?>">Komoditas</a>
-            </li>
-            <div class="vertical-line"></div>
-            <li>
-                <a href="index.php?route=/statistik"
-                    class="nav-link <?= ($current_page == 'statistik') ? 'active' : ''; ?>">Statistik</a>
-            </li>
-            <div class="vertical-line"></div>
-            <li>
-                <a href="index.php?route=/admin" class="nav-link">Panel Admin</a>
-            </li>
-        </ul>
-    </nav>
 </header>
